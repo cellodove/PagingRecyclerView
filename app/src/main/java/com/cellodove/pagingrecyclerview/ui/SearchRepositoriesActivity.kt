c4a779be647d073ac42fc08a0a4b36211dc136ad
jusung.kin@skylabs.io <wntjd200@naver.com>
2021-12-13 17:58:20 +0900

    i made activity

--- app/src/main/java/com/cellodove/pagingrecyclerview/ui/SearchRepositoriesActivity.kt
+++ app/src/main/java/com/cellodove/pagingrecyclerview/ui/SearchRepositoriesActivity.kt
@@ -2,6 +2,8 @@ package com.cellodove.pagingrecyclerview.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
 import com.cellodove.pagingrecyclerview.databinding.ActivitySeachRepositoriesBinding
 
 class SearchRepositoriesActivity : AppCompatActivity() {
@@ -10,6 +12,26 @@ class SearchRepositoriesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-    }
 
+        binding.searchRepo.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                binding.searchRepo.text
+                true
+            } else {
+                false
+            }
+        }
+
+        binding.searchRepo.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                binding.searchRepo.text
+                true
+            } else {
+                false
+            }
+        }
+
+
+
+    }
 }
